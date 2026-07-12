0b825d560392039748564ccf577095dbe78d454b
GitHub Action <action@github.com>
2026-07-12 01:52:53 +0000

    chore: update course data

--- data/CourseInfoData.ts
+++ data/CourseInfoData.ts
@@ -9,7 +9,7 @@ export type CourseInfoDataType = {
 
 export const CourseInfoData: CourseInfoDataType = {
   "metadata": {
-    "generated": "2026-07-05T02:10:36.704Z"
+    "generated": "2026-07-12T01:52:53.457Z"
   },
   "courses": [
     {
